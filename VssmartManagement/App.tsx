26ea9d643f84effb583e97295a0424cb1fe2663b
gianghiem0310 <jangiim93nt@gmail.com>
2024-07-05 14:19:38 +0700

    Main Source

---
+++ VssmartManagement/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@ant-design/react-native";
+import React from "react";
+const App = () => {
+  return (
+    <Button type="primary"> Lets</Button>
+  );
+}
+
+export default App;
---
+++ VssmartManagement/src/components/Header.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet, View,Dimensions } from "react-native";
+const { width } = Dimensions.get('window');
+const Header = () => {
+    return (
+        
+        <View style={styles.header}>
+                
+        </View>
+        
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    header: {
+        height: 20,                   // 20 pixels
+        width: width,                 // chiều rộng bằng màn hình
+        backgroundColor: 'blue',      // màu nền xanh
+    }
+})
+export default Header;
